e1fbfc8421082e03c115959f88fa89442aaaeb1f
Diego <dglvr3@gmail.com>
2026-05-26 15:55:57 -0300

    feat: add spring jpa and mysql connector dependencies, add database connection in application.yml and add database mapping inside Anime class

--- src/main/java/com/diego/projetos/springboot_studies/domain/Anime.java
+++ src/main/java/com/diego/projetos/springboot_studies/domain/Anime.java
@@ -1,11 +1,22 @@
 package com.diego.projetos.springboot_studies.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data // junction of @ToString, @EqualsAndHashCode, @Getter/@Setter and @RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor // generate an empty constructor, necessary for using @Entity (JPA pattern)
+@Entity // specifies that this class maps to a db table
 public class Anime {
+    @Id // specifies that id is used as primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // used alongside @Id to make id auto-generated
+    // search for generation types later
     private Long id;
     private String name;
 //    @JsonProperty("name")

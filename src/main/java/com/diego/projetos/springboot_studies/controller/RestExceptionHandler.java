e01613bd2d8ad77eff5cc527d466150924250a52
Diego <dglvr3@gmail.com>
2026-05-27 16:37:59 -0300

    feat: add global BadRequestException handler using @ControllerAdvice and @ExceptionHandler annotations

---
+++ src/main/java/com/diego/projetos/springboot_studies/controller/RestExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.diego.projetos.springboot_studies.controller;
+
+import com.diego.projetos.springboot_studies.exception.BadRequestException;
+import com.diego.projetos.springboot_studies.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    // what is happening here is: if any of the Controllers in my project get
+    // a BadRequestException, it will use the @ExceptionHandler and return
+    // the value that you defined inside the method with the annotation
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails>
+            badRequestExceptionHandler(BadRequestException badRequestException) {
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception. Check the Documentation")
+                        .details(badRequestException.getMessage())
+                        .developerMessage(badRequestException.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST
+        );
+    }
+}

350f3a48b2910d8d0a97b9b719978077d17ae690
Diego <dglvr3@gmail.com>
2026-06-05 18:18:01 -0300

    feat: configure HTTP Basic authentication and disable CSRF

--- src/main/java/com/diego/projetos/springboot_studies/config/SecurityConfiguration.java
+++ src/main/java/com/diego/projetos/springboot_studies/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,23 +12,40 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 //@EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // note 1
+                .csrf(AbstractHttpConfigurer::disable) // note 2
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults()); // note 3
+
         return http.build();
     }
     // states that any HTTP request must be authenticated
     // basically it defines the security laws of the application
 
+    // note 1:
+    // we could generate a CSFR token as showed below, but not going to because it makes testing difficult
+    // in real applications, it's the preferred way though
+    // .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+    // CookieCsrfTokenRepository doesn't create a token, instead it defines:
+    // "if there is a token, it will be stored in a cookie"
+
+    // note 2:
+    // same as csrf.disable(), but with method reference
+
+    // note 3:
+    // Customizer.withDefaults enables HTTP Basic authentication with default Spring Security configuration
+
     @Bean
-    public PasswordEncoder passwordEncoder () {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
         // note: PasswordEncoder -> interface
         // BCryptPasswordEncoder -> implementation
@@ -41,7 +59,7 @@ public class SecurityConfiguration {
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         UserDetails admin = User.withUsername("diego")
-                .password(passwordEncoder.encode("diego123")) // wrap the password inside the encoder
+                .password(passwordEncoder.encode("diego123")) // wraps the password inside the encoder
                 .roles("USER", "ADMIN")
                 .build();
 
